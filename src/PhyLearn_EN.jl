d47ed3f573f168b0b0c9541bdf33285cf12be509
Yue <yueshang@sas.upenn.edu>
2025-11-28 15:24:50 -0500

    update

--- src/PhyLearn_EN.jl
+++ src/PhyLearn_EN.jl
@@ -2,7 +2,9 @@ module PhyLearn_EN
 
 include("ENM.jl")
 include("CL.jl")
-export ENM,load_graph,reset_config!,run_md!,calc_elastic_jacobian,calc_modes,Trainer_CL,calc_strain_f,step!
+export ENM,reset_config!,run_md!,cal_elastic_energy,cal_kinetic_energy,cal_strain,put_stain!,
+        quench_fire,calc_elastic_jacobian,calc_modes,
+        Trainer_CL,calc_strain_f,step!
 
 
 
