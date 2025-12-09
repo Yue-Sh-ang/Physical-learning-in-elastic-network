637713bf4c502801b3259fe5d652a73c544bbf3b
Yue <yueshang@sas.upenn.edu>
2025-12-09 10:43:29 -0500

    include basin_hopping method

--- src/PhyLearn_EN.jl
+++ src/PhyLearn_EN.jl
@@ -2,9 +2,11 @@ module PhyLearn_EN
 
 include("ENM.jl")
 include("CL.jl")
+include("Basin_hoping.jl")
 export ENM,reset_config!,run_md!,cal_elastic_energy,cal_kinetic_energy,cal_strain,put_stain!,plot_net,cal_degree,
         quench_fire!,cal_elastic_jacobian,cal_modes,
-        Trainer_CL,step!
+        Trainer_CL,step!,
+        basin_hopping!
 
 
 
